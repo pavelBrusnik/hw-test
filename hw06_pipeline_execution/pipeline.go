c7aa111a24a79f3b7b77cd1245e5bc2b9336b4f7
Павел Павлович Брусник <pavel.brusnik@vseinstrumenti.ru>
2023-01-31 23:08:15 +0300

    HW06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := do(in, done)
+	for _, stage := range stages {
+		if stage != nil {
+			out = stage(do(out, done))
+		} else {
+			panic("stage is nil!")
+		}
+	}
+
+	return out
+}
+
+func do(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+
+	return out
 }
